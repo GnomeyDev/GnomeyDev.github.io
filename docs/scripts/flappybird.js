6d0ccad36757f6e1cf4197461321f8d8e5ec48ca
GnomeyDev <quucksponsers@gmail.com>
2022-06-26 13:22:59 +0100

    skgdf

--- docs/scripts/flappybird.js
+++ docs/scripts/flappybird.js
@@ -24,7 +24,39 @@ var myGameArea = {
     }
 }
 
-
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.score = 0;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
+            this.gravitySpeed = 0;
+        }
+    }
     this.crashWith = function(otherobj) {
         var myleft = this.x;
         var myright = this.x + (this.width);
@@ -40,7 +72,7 @@ var myGameArea = {
         }
         return crash;
     }
-
+}
 
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
