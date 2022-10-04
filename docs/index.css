7b52404a52691c73bfbeab80c6160657da0cb30b
GnomeyDev <106401515+GnomeyDev@users.noreply.github.com>
2022-10-04 19:51:41 +0100

    Update index.css

--- docs/index.css
+++ docs/index.css
@@ -1,1 +1,65 @@
+html, body {
+  /*padding: 10px;*/
+}
+body{
+  font-family: "Lucida Console", "Courier New", monospace;
+  font-size: 40px;
+}
+p{
+  font-size: 20px;
+}
+a{
+  text-decoration: none;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  display:inline-block;
+  width: 90%;
+  text-align: center;
+  
+  borde-radius: 10px;
+  border-width: 5px;
+  border-style: solid;
+  
+  background-color: red;
+}
+
+.card{
+  margin: 10px;
+  padding: 10px;
+  
+  border-radius: 10px;
+  border-width: 10px;
+  border-style: solid;
+}
+.cards{
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.promo{
+  background-color: rgb(28,165,40);
+  border-color: rgb(19,163,32);
+}
+.promo a{
+  background-color: rgb(158,27,27);
+  border-color: rgb(123,23,23);
+}
+.promo a:hover{
+  background-color: rgb(155,41,41);
+  border-color: rgb(112,15,15);
+}
+
+.repos{
+  background-color: rgb(28,165,155);
+  border-color: rgb(28,159,174);
+}
+.repos a{
+  background-color: rgb(158,82,27);
+  border-color: rgb(123,82,23);
+}
+.repos a:hover{
+  background-color: rgb(155,89,41);
+  border-color: rgb(112,73,15;
+}
 
